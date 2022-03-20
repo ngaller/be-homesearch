d1f02c3997dc16efb31ca3cecace55f55106f0b9
Nicolas Galler <ngaller@gmail.com>
2022-03-20 07:52:51 +0100

    Rescore should pick up synced homes

--- localize_be/tasks/score_homes.py
+++ localize_be/tasks/score_homes.py
@@ -3,6 +3,7 @@ from contextlib import closing
 import pandas as pd
 from prefect import task
 
+from localize_be.config import logger
 from localize_be.core.scoring import calculate_score
 from localize_be.resources.home_cache import get_home_cache
 
@@ -11,6 +12,7 @@ from localize_be.resources.home_cache import get_home_cache
 def score_all(pois: pd.DataFrame, rescore: bool = False):
     with closing(get_home_cache()) as home_cache:
         homes = home_cache.get_homes_to_sync() if not rescore else home_cache.get_homes_synced()
+        logger.debug(f"Rescoring {len(homes)} homes")
         for id_, details in homes:
             calculate_score(details, pois)
             home_cache.update_home(id_, details)
